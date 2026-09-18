1e35268cce42974996a67fbefe918f45744065cd
yascoder008 <yasmineimatouken2008@gmail.com>
2026-09-18 20:17:42 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 function App(){
   return(
     <CartProvider>
-     <Router>
+     <Router basename="Shoe-Store-LENOVA">
       <Nav/>
       <Routes>
         <Route path="/" element={<Home/>}/>
